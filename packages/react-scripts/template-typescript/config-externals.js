4f539219cd65ad3cda84f762c6ac2a835fbd598e
Steffen Ritter <steffen.ritter@my-rio.de>
2019-05-17 14:37:05 +0200

    updated app navigation once again

--- packages/react-scripts/template-typescript/config-externals.js
+++ packages/react-scripts/template-typescript/config-externals.js
@@ -20,7 +20,7 @@ module.exports = [
         module: 'rio-accountmenu',
     },
     {
-        entry: 'https://appnavigator.developers.rio.cloud/1.0.3/rio-appnavigator.js',
+        entry: 'https://appnavigator.developers.rio.cloud/1.0.4/rio-appnavigator.js',
         global: 'RioAppNavigator',
         module: 'rio-appnavigator',
     },
