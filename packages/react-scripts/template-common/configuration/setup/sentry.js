d1f098c30d58d9831d0f5b4331923208c781730c
rkipsch <git-smk@outlook.com>
2019-07-17 15:29:16 +0200

    (fix) sentry setup which required an env config

--- packages/react-scripts/template-common/configuration/setup/sentry.js
+++ packages/react-scripts/template-common/configuration/setup/sentry.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import * as Sentry from '@sentry/browser';
-import { env } from '../env';
+
+const SENTRY_DNS = '<sntry-dns-token>';
 
 // version and environment are defined in the webpack.define plugin
 const release = VERSION;
@@ -9,13 +10,11 @@ const environment = ENVIRONMENT;
 // should have been called before using it here
 // ideally before even rendering your react app
 Sentry.init({
-    dsn: env.runtimeConfig.sentryToken,
+    dsn: SENTRY_DNS,
     environment,
     release,
 });
 
 export const reportErrorToSentry = (...args) => {
-    if (env.shouldSendMetrics) {
-        Sentry.captureException(...args);
-    }
+    Sentry.captureException(...args);
 };
