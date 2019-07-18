512b89be8e5fdba05f7ed39014c26b3178380a68
Steffen Ritter <steffen.ritter@my-rio.de>
2019-07-18 16:41:09 +0200

    fixed ts reducer setup

--- packages/react-scripts/template-typescript/src/reducers.ts
+++ packages/react-scripts/template-typescript/src/reducers.ts
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux';
-
+import {History} from "history";
 import { configReducer, langReducer, loginReducer, sessionReducer /*, tokenHandlingReducer*/ } from './configuration';
 import tokenHandlingReducer from './configuration/tokenHandling/reducer'; // TODO make import from ./configuration work
 import appReducer from './features/app/reducers/App.reducers';
+import {connectRouter} from "connected-react-router";
 
-export const rootReducer = combineReducers({
+export const rootReducer = (history: History) =>  combineReducers({
     config: configReducer,
     lang: langReducer,
     app: appReducer,
     login: loginReducer,
     session: sessionReducer,
+    router: connectRouter(history),
     tokenHandling: tokenHandlingReducer,
 });
